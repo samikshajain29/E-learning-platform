ef7ed2f5fdad93357081c06e75aa673865acc4c6
Samiksha Jain <samikshaj7371@gmail.com>
2025-09-11 19:39:12 +0530

    setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
