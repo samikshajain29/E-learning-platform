9b96be7ba082ab72f96d2b909f2dc365337bee60
Samiksha Jain <samikshaj7371@gmail.com>
2025-09-11 18:35:46 +0530

    Setup authentication

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,5 +1,7 @@
 import User from "../models/userModel.js";
 import validator from "validator";
+import bcrypt from "bcryptjs";
+import genToken from "../config/token.js";
 
 export const signup = async (req, res) => {
   try {
@@ -14,5 +16,56 @@ export const signup = async (req, res) => {
     if (password.length < 8) {
       return res.status(400).json({ message: "Enter Strong password" });
     }
-  } catch (error) {}
+    let hashPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      email,
+      password: hashPassword,
+      role,
+    });
+
+    let token = await genToken(user._id);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "Strict",
+      maxAge: 10 * 365 * 24 * 60 * 60 * 1000,
+    });
+    return res.status(201).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: `Signup error ${error}` });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    let isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Incorrect password" });
+    }
+    let token = await genToken(user._id);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "Strict",
+      maxAge: 10 * 365 * 24 * 60 * 60 * 1000,
+    });
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: `Login error ${error}` });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    await res.clearCookie("token");
+    return res.status(200).json({ message: `Logout Successfully` });
+  } catch (error) {
+    return res.status(500).json({ message: `Logout error ${error}` });
+  }
 };
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/authRoutes.js";
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
   console.log("server started");
