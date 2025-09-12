e5e597c045fb2d0648508611b177d70469cf89aa
Samiksha Jain <samikshaj7371@gmail.com>
2025-09-12 22:06:03 +0530

    Create signup and login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import logo from "../assets/logo.jpg";
+import google from "../assets/google.jpg";
+import { IoEyeOutline } from "react-icons/io5";
+import { IoEye } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-  return <div></div>;
+  const [show, setShow] = useState(false);
+  const navigate = useNavigate();
+  return (
+    <div className="bg-[#dddbdb] w-[100vw] h-[100vh] flex items-center justify-center">
+      <form className="w-[90%] md:w-[800px] h-[600px] bg-[white] shadow-xl rounded-2xl flex">
+        {/* left div */}
+        <div className="md:w-[50%] w-[100%] h-[100%] flex flex-col items-center justify-center gap-3">
+          <div>
+            <h1 className="font-semibold text-[black] text-2xl">
+              Welcome back
+            </h1>
+            <h2 className="text-[#999797] text-[18px]">Login your account</h2>
+          </div>
+          {/* form for signup */}
+          <div className="flex flex-col gap-1 w-[80%] items-start justify-center px-3">
+            <label htmlFor="email" className="font-semibold">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              className="border-2 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px]"
+              placeholder="Your Email"
+            />
+          </div>
+          <div className="flex flex-col gap-1 w-[80%] items-start justify-center px-3 relative">
+            <label htmlFor="password" className="font-semibold">
+              Password
+            </label>
+            <input
+              id="password"
+              type={show ? "text" : "password"}
+              className="border-2 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px]"
+              placeholder="Your Password"
+            />
+            {!show ? (
+              <IoEyeOutline
+                className="absolute w-[20px] h-[20px] cursor-pointer right-[6%] bottom-[10%]"
+                onClick={() => setShow((prev) => !prev)}
+              />
+            ) : (
+              <IoEye
+                className="absolute w-[20px] h-[20px] cursor-pointer right-[6%] bottom-[10%]"
+                onClick={() => setShow((prev) => !prev)}
+              />
+            )}
+          </div>
+          <button className="w-[80%] h-[40px] bg-black text-white cursor-pointer flex items-center justify-center rounded-[5px]">
+            Login
+          </button>
+          <span className="text-[13px] cursor-pointer text-[#585757]">
+            Forget your password ?
+          </span>
+          {/* for line and continue */}
+          <div className="w-[80%] flex items-center gap-2">
+            <div className="w-[25%] h-[0.5px] bg-[#c4c4c4]"></div>
+            <div className="w-[50%] text-[15px] text-[#6f6f6f] flex items-center justify-center">
+              or continue
+            </div>
+            <div className="w-[25%] h-[0.5px] bg-[#c4c4c4]"></div>
+          </div>
+          <div className="w-[80%] h-[40px] border-2 border-[gray] rounded-[5px] flex items-center justify-center">
+            <img src={google} alt="google" className="w-[25px]" />
+            <span className="text-[18px] text-gray-500">oogle</span>
+          </div>
+          <div className="text-[#6f6f6f] cursor-pointer">
+            Don't have an account?
+            <span
+              className="underline underline-offset-1 text-[black]"
+              onClick={() => navigate("/signup")}
+            >
+              Signup
+            </span>
+          </div>
+        </div>
+        {/* right div */}
+        <div className="w-[50%] h-[100%] rounded-r-2xl bg-[black] md:flex items-center justify-center flex-col hidden">
+          <img
+            src={logo}
+            alt="logo"
+            className="w-[120px] h-[120px] rounded-full shadow-2xl"
+          />
+          <span className="text-2xl mt-[5px] text-white">E-LEARNING</span>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
+import logo from "../assets/logo.jpg";
+import google from "../assets/google.jpg";
+import { IoEyeOutline } from "react-icons/io5";
+import { IoEye } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
-  return <div></div>;
+  const [show, setShow] = useState(false);
+  const navigate = useNavigate();
+  return (
+    <div className="bg-[#dddbdb] w-[100vw] h-[100vh] flex items-center justify-center">
+      <form className="w-[90%] md:w-[800px] h-[600px] bg-[white] shadow-xl rounded-2xl flex">
+        {/* left div */}
+        <div className="md:w-[50%] w-[100%] h-[100%] flex flex-col items-center justify-center gap-3">
+          <div>
+            <h1 className="font-semibold text-[black] text-2xl">
+              Let's get started
+            </h1>
+            <h2 className="text-[#999797] text-[18px]">Create your account</h2>
+          </div>
+
+          {/* form for signup */}
+          <div className="flex flex-col gap-1 w-[80%] items-start justify-center px-3">
+            <label htmlFor="name" className="font-semibold">
+              Name
+            </label>
+            <input
+              id="name"
+              type="text"
+              className="border-2 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px]"
+              placeholder="Your Name"
+            />
+          </div>
+          <div className="flex flex-col gap-1 w-[80%] items-start justify-center px-3">
+            <label htmlFor="email" className="font-semibold">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              className="border-2 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px]"
+              placeholder="Your Email"
+            />
+          </div>
+          <div className="flex flex-col gap-1 w-[80%] items-start justify-center px-3 relative">
+            <label htmlFor="password" className="font-semibold">
+              Password
+            </label>
+            <input
+              id="password"
+              type={show ? "text" : "password"}
+              className="border-2 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px]"
+              placeholder="Your Password"
+            />
+            {!show ? (
+              <IoEyeOutline
+                className="absolute w-[20px] h-[20px] cursor-pointer right-[6%] bottom-[10%]"
+                onClick={() => setShow((prev) => !prev)}
+              />
+            ) : (
+              <IoEye
+                className="absolute w-[20px] h-[20px] cursor-pointer right-[6%] bottom-[10%]"
+                onClick={() => setShow((prev) => !prev)}
+              />
+            )}
+          </div>
+          <div className="flex md:w-[50%] w-[70%] items-center justify-between">
+            <span className="px-[10px] py-[5px] border-[2px] border-[#e7e6e6] rounded-xl cursor-pointer hover:border-black">
+              Student
+            </span>
+            <span className="px-[10px] py-[5px] border-[2px] border-[#e7e6e6] rounded-xl cursor-pointer hover:border-black">
+              Educator
+            </span>
+          </div>
+          <button className="w-[80%] h-[40px] bg-black text-white cursor-pointer flex items-center justify-center rounded-[5px]">
+            Signup
+          </button>
+          {/* for line and continue */}
+          <div className="w-[80%] flex items-center gap-2">
+            <div className="w-[25%] h-[0.5px] bg-[#c4c4c4]"></div>
+            <div className="w-[50%] text-[15px] text-[#6f6f6f] flex items-center justify-center">
+              or continue
+            </div>
+            <div className="w-[25%] h-[0.5px] bg-[#c4c4c4]"></div>
+          </div>
+          <div className="w-[80%] h-[40px] border-2 border-[gray] rounded-[5px] flex items-center justify-center">
+            <img src={google} alt="google" className="w-[25px]" />
+            <span className="text-[18px] text-gray-500">oogle</span>
+          </div>
+          <div className="text-[#6f6f6f] cursor-pointer">
+            Already have an account?
+            <span
+              className="underline underline-offset-1 text-[black]"
+              onClick={() => navigate("/login")}
+            >
+              Login
+            </span>
+          </div>
+        </div>
+        {/* right div */}
+        <div className="w-[50%] h-[100%] rounded-r-2xl bg-[black] md:flex items-center justify-center flex-col hidden">
+          <img
+            src={logo}
+            alt="logo"
+            className="w-[120px] h-[120px] rounded-full shadow-2xl"
+          />
+          <span className="text-2xl mt-[5px] text-white">E-LEARNING</span>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
