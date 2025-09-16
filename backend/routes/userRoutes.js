2627d35c166973eef6f5ea720de88d912bea10e8
Samiksha Jain <samikshaj7371@gmail.com>
2025-09-16 21:45:49 +0530

    create update profile controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { getCurrentUser } from "../controllers/userControllers.js";
+import {
+  getCurrentUser,
+  updateProfile,
+} from "../controllers/userControllers.js";
 import isAuth from "../middlewares/isAuth.js";
+import upload from "../middlewares/multer.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/getcurrentuser", isAuth, getCurrentUser);
+userRouter.post(
+  "/updateprofile",
+  isAuth,
+  upload.single("photoUrl"),
+  updateProfile
+);
 
 export default userRouter;
