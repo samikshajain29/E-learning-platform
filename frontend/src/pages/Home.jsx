39e5d2e4272e3f46698bda0359277df2667831af
Samiksha Jain <samikshaj7371@gmail.com>
2025-09-17 13:43:14 +0530

    create explore course component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { SiViaplay } from "react-icons/si";
 import ai from "../assets/ai.png";
 import ai1 from "../assets/SearchAi.png";
 import Logos from "../component/Logos";
+import ExploreCourses from "../component/ExploreCourses";
 
 function Home() {
   return (
@@ -43,6 +44,7 @@ function Home() {
         </div>
       </div>
       <Logos />
+      <ExploreCourses />
     </div>
   );
 }
