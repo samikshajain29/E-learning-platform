d0b3a4393c2f501189991e351b25b12bc51dbb37
Samiksha Jain <samikshaj7371@gmail.com>
2025-11-26 22:07:24 +0530

    create graph in dashboard

--- frontend/src/pages/Educator/Dashboard.jsx
+++ frontend/src/pages/Educator/Dashboard.jsx
@@ -2,10 +2,39 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { FaArrowLeftLong } from "react-icons/fa6";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 function Dashboard() {
   const { userData } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const { creatorCourseData } = useSelector((state) => state.course);
+
+  const courseProgressData =
+    creatorCourseData?.map((course) => ({
+      name: course.title?.slice(0, 10) + "...",
+      lectures: course.lectures?.length || 0,
+    })) || [];
+
+  const EnrollData =
+    creatorCourseData?.map((course) => ({
+      name: course.title?.slice(0, 10) + "...",
+      enrolled: course.enrolledStudents?.length || 0,
+    })) || [];
+
+  const totalEarning =
+    creatorCourseData?.reduce((sum, course) => {
+      const studentCount = course.enrolledStudents?.length || 0;
+      const courseRevenue = course.price ? course.price * studentCount : 0;
+      return sum + courseRevenue;
+    }, 0) || 0;
   return (
     <div className="flex min-h-screen bg-gray-100">
       <FaArrowLeftLong
@@ -25,7 +54,7 @@ function Dashboard() {
               Welcome, {userData?.name || "Educator"} 👋
             </h1>
             <h1 className="text-xl font-semibold text-gray-800">
-              Total Earning : 0
+              Total Earning : ₹{totalEarning.toLocaleString()}
             </h1>
             <p className="text-gray-600 text-sm">
               {userData?.description ||
@@ -40,7 +69,37 @@ function Dashboard() {
           </div>
         </div>
         {/* Graph Section */}
-        <div></div>
+        <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
+          {/* for course Progress graph */}
+
+          <div className="bg-white rounded-lg shadow-md p-6">
+            <h2 className="text-lg font-semibold mb-4">
+              Course Progress (Lectures)
+            </h2>
+            <ResponsiveContainer width="100%" height={300}>
+              <BarChart data={courseProgressData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey="lectures" fill="black" radius={[5, 5, 0, 0]} />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+          {/* Enrolled Data */}
+          <div className="bg-white rounded-lg shadow-md p-6">
+            <h2 className="text-lg font-semibold mb-4">Students Enrollment</h2>
+            <ResponsiveContainer width="100%" height={300}>
+              <BarChart data={EnrollData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey="enrolled" fill="black" radius={[5, 5, 0, 0]} />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
       </div>
     </div>
   );
