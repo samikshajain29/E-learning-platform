5309a493448900a75c3ce03f273f280a010a8c56
Samiksha Jain <samikshaj7371@gmail.com>
2025-12-02 15:13:57 +0530

    update review card

--- frontend/src/component/ReviewCard.jsx
+++ frontend/src/component/ReviewCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaStar } from "react-icons/fa";
 import { FaRegStar } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 function ReviewCard({
   comment,
@@ -10,6 +11,7 @@ function ReviewCard({
   description,
   courseTitle,
 }) {
+  const { userData } = useSelector((state) => state.user);
   return (
     <div className="bg-white p-6 rounded-xl shadow-md hover:shadow-lg transition-all duration-300 max-w-sm w-full">
       <div className="flex items-center mb-3 text-yellow-400 text-sm">
@@ -26,11 +28,16 @@ function ReviewCard({
         Review: <span className="font-semibold">{comment}</span>
       </p>
       <div className="flex items-center gap-2">
-        <img
-          src={photoUrl}
-          className="w-10 h-10 rounded-full object-cover"
-          alt=""
-        />
+        {photoUrl ? (
+          <img
+            src={photoUrl}
+            className="w-10 h-10 rounded-full object-cover "
+          />
+        ) : (
+          <div className="w-10 h-10 rounded-full text-white flex items-center justify-center text-[20px] border-2 border-white bg-black cursor-pointer">
+            {userData?.name?.slice(0, 1).toUpperCase()}
+          </div>
+        )}
         <div>
           <h2 className="font-semibold text-gray-800 text-sm">{name}</h2>
           <p className="text-xs text-gray-500">{description}</p>
