51df2647869b1f1ebb367d9ac70babfebcdd9c15
Samiksha Jain <samikshaj7371@gmail.com>
2026-05-13 15:46:22 +0530

    Update CORS origin to include admin panel

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://e-learning-platform-frontend-dl4b.onrender.com", "http://localhost:5174"],
+    origin: ["https://e-learning-platform-frontend-dl4b.onrender.com", "https://e-learning-platform-adminpanel.onrender.com"],
     credentials: true,
   })
 );
