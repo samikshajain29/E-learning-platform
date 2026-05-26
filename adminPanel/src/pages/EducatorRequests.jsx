c14a68d3f65feddbb5e6a6d6c8252fc116fbaba1
Samiksha Jain <samikshaj7371@gmail.com>
2026-05-26 16:59:02 +0530

    Update API_URL to production server

--- adminPanel/src/pages/EducatorRequests.jsx
+++ adminPanel/src/pages/EducatorRequests.jsx
@@ -6,7 +6,7 @@ import {
   BadgeCheck
 } from "lucide-react";
 
-const API_URL = (import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : "http://localhost:8000/api");
+const API_URL = "https://e-learning-platform-server-dgpe.onrender.com/api";
 
 const EducatorRequests = () => {
   const [requests, setRequests] = useState([]);
